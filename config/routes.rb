03b956194d0d6921b6cc1829cc7f9f70f39d6ac0
RicardoG2016 <gomez.r@live.com>
2017-08-08 01:10:50 -0500

    setup landing page when not logged in also setup bootstrap and material ui theme with email contact us section.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'groups/index'
 
   resources :groups, :events
-  
+
   resources :posts do
     collection do 
       get 'search'
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'posts#index'
+  root 'home#index'
 end
