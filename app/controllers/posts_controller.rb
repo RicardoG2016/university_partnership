65a7b1bdadf5b7b6cb04224e11b978f6f48c064b
RicardoG2016 <gomez.r@live.com>
2017-08-08 03:22:11 -0500

    sign up form styled. Home page setup as landing and redirects to posts when signed in. Style revisions required to correct some alignements.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy] 
-  before_action :authenticate_group!, except: [:index, :show]
+  before_action :authenticate_group!
 
 def search
   if params[:search].present?
