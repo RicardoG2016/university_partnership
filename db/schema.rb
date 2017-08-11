f594ef36b2fa5406adef1b00a48b740cc560dcd5
RicardoG2016 <gomez.r@live.com>
2017-08-11 18:38:07 -0500

    commit to switch branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170811021130) do
 
   create_table "chats", force: :cascade do |t|
@@ -18,7 +17,7 @@ ActiveRecord::Schema.define(version: 20170811021130) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.integer "post_id"
