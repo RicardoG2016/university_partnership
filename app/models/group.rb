b0a066b65ddb3f32d35052591c2482377d5c46ca
RicardoG2016 <gomez.r@live.com>
2017-08-10 21:15:36 -0500

    setup chat, message and subscription models and associations.

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,6 +5,20 @@ class Group < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts, dependent: :destroy
-  has_many :events, dependent: :destroy 
+  has_many :events, dependent: :destroy
+  has_many :messages
+  has_many :subscriptions
+  has_many :chats, through: :subscriptions
+
+  def existing_chat_groups
+    existing_chat_groups = []
+    self.chats.each do |chat|
+      existing_chat_groups.concat(chat.subscriptions.where.not(group_id: self.id).map { |subscription| subscription.group} )
+    end
+
+    existing_chat_groups.uniq
+  end
+
+
   validates :university, presence: true, uniqueness: true    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807185839) do
+ActiveRecord::Schema.define(version: 20170811021130) do
+
+  create_table "chats", force: :cascade do |t|
+    t.string "identifier"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -55,12 +61,43 @@ ActiveRecord::Schema.define(version: 20170807185839) do
     t.index ["reset_password_token"], name: "index_groups_on_reset_password_token", unique: true
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.integer "group_id"
+    t.integer "chat_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.integer "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_posts_on_group_id"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer "chat_id"
+    t.integer "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.string "votable_type"
+    t.integer "votable_id"
+    t.string "voter_type"
+    t.integer "voter_id"
+    t.boolean "vote_flag"
+    t.string "vote_scope"
+    t.integer "vote_weight"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+    t.index ["votable_type", "votable_id"], name: "index_votes_on_votable_type_and_votable_id"
+    t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
+    t.index ["voter_type", "voter_id"], name: "index_votes_on_voter_type_and_voter_id"
   end
 
 end
